eaf4b33d06ddab3842104545062340d93aac0ab0
Thomas Oliver <oliver.tommy@icloud.com>
2024-10-18 15:03:10 +0000

    Add get API key status

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+const API_KEY = "Cnrzsn9OFlKPpyanEGh4yh-zGsw";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class = "no_errors>No errors reported!</div>`
+    } else {
+        results = `<div>Total Errors: ${data.total_errors}</div>`
+        for (let error of data.error_list) {
+            results += `<div>At line ${error.line}, `;
+            results += `column ${error.col}</div>`;
+            resukts += `<div>${error.error}</div>`;
+        }
+    }
+}
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Status";
+    document.getElementById("results-content").innerHTML = `<p>Your key is valid until: ${data.expiry}</p>`;
+    resultsModal.show();
+}
